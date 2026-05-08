1958f4ae2c71d6e376e4415fbe5670ee1cb5e825
oldmartijntje <oldmartijntje@gmail.com>
2026-05-08 11:20:49 +0200

    i forgor

--- huiswerk/src/main/java/library/datastorage/MySQLMemberDAO.java
+++ huiswerk/src/main/java/library/datastorage/MySQLMemberDAO.java
@@ -27,7 +27,7 @@ public class MySQLMemberDAO implements IMemberDAO {
         aBook.addCopy(copyMulisch);
         books.add(bookMulisch);
 
-        Member aNewMember = new Member(1000, "Henk", "Hopper, de", new BurgerServiceNummer("fake-id"));
+        Member aNewMember = new Member(1000, "Henk", "Hopper, de", "fake-id" , "NL");
 
         // Initialize a calendar object with today as the current day.
         Calendar c = Calendar.getInstance();
@@ -38,7 +38,7 @@ public class MySQLMemberDAO implements IMemberDAO {
         aNewMember.addLoan(new Loan(aNewMember, copyMulisch, c.getTime()));
         members.add(aNewMember);
 
-        aNewMember = new Member(1001, "Mara", "Machtig", new BurgerServiceNummer("fake-id"));
+        aNewMember = new Member(1001, "Mara", "Machtig", "fake-id", "DE");
         members.add(aNewMember);
         c.setTime(dateToday);
         c.add(Calendar.DATE, 8);
@@ -48,18 +48,18 @@ public class MySQLMemberDAO implements IMemberDAO {
         aReservation.setReservationDate(c.getTime());
         aNewMember.addReservation(aReservation);
 
-        aNewMember = new Member(1002, "Kees", "Kerfstok", new BurgerServiceNummer("fake-id"));
+        aNewMember = new Member(1002, "Kees", "Kerfstok", "fake-id", "BE");
         members.add(aNewMember);
         aReservation = new Reservation(aNewMember, bookMulisch);
         c.setTime(dateToday);
         aReservation.setReservationDate(c.getTime());
         aNewMember.addReservation(aReservation);
 
-        aNewMember = new Member(1003, "Anna", "Nas", new BurgerServiceNummer("fake-id"));
+        aNewMember = new Member(1003, "Anna", "Nas", "fake-id", "DE");
         aNewMember.setFine(5.25);
         members.add(aNewMember);
 
-        members.add(new Member(1004, "Cor", "Netto", new BurgerServiceNummer("fake-id")));
+        members.add(new Member(1004, "Cor", "Netto", "fake-id", "NL"));
     }
 
     public Member FindMember(int identifier) {
